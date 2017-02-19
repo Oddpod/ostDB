df5a4387b1331b7b7b519e5e0522ad7c7cb98c1e
Odd <oddh123@gmail.com>
2017-02-19 16:35:42 +0100

    Added filter field to list and batch add from textfile option

--- app/src/main/java/com/example/odd/ostrino/AnimuOst.java
+++ app/src/main/java/com/example/odd/ostrino/AnimuOst.java
@@ -3,31 +3,29 @@ package com.example.odd.ostrino;
 import android.app.DialogFragment;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
-import android.graphics.Color;
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TableLayout;
-import android.widget.TableRow;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import java.util.List;
+import java.util.Scanner;
 
 
 public class AnimuOst extends AppCompatActivity implements AddScreen.AddScreenListener {
 
-    Button btnAdd, btnViewList;
+    Button btnAdd, btnViewList, btnAddOsts;
     SQLiteDatabase dtb;
     private String TAG = "OstInfo";
     DBHandler db;
-    ListScreen ls;
+    List<Ost> ostList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,11 +34,11 @@ public class AnimuOst extends AppCompatActivity implements AddScreen.AddScreenLi
 
         db = new DBHandler(this);
         //Reset database
-        //SQLiteDatabase dbS = db.getWritableDatabase();
         //db.onUpgrade(dbS, 1, 2);
 
         btnAdd = (Button) findViewById(R.id.btnAddOst);
         btnViewList = (Button) findViewById(R.id.btnViewOstList);
+        btnAddOsts = (Button) findViewById(R.id.btnBatchAdd);
 
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,6 +54,35 @@ public class AnimuOst extends AppCompatActivity implements AddScreen.AddScreenLi
                 //View Block Number List in the Text View Widget
                 ListScreen ls = new ListScreen();
                 launchListScreen();
+            }
+        });
+
+        btnAddOsts.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                ostList = db.getAllOsts();
+                dtb = db.getWritableDatabase();
+                try {
+                    InputStream is = getResources().getAssets().open("Osts.txt");
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+                    String line;
+                    while((line = reader.readLine()) != null) {
+                        Ost ost = new Ost();
+                        String[] lineArray = line.split("; ");
+                        ost.setTitle(lineArray[0]);
+                        ost.setShow(lineArray[1]);
+                        ost.setTags(lineArray[2]);
+                        ost.setUrl(lineArray[3]);
+                        if (ostList.contains(ost)){
+                            System.out.println( ost);
+                            db.addNewOst(ost);
+                        }
+                    }
+                }catch (java.io.IOException e){
+                    System.out.println("File not found");
+                }
+
             }
         });
         }
